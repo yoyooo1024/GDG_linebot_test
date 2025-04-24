07ca94b2f277d46e4c765a0212edb70cd941e9be
yoyooo1024 <yoyo921024@gmail.com>
2025-04-24 20:03:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ from linebot.exceptions import InvalidSignatureError
 import logging
 from places import get_nearby_restaurants
 from stock import txt_to_img_url
+import google.generativeai as genai
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-pro")
 
 # 加載 .env 文件中的變數
 load_dotenv()
@@ -92,8 +95,8 @@ def handle_message(event: Event):
                 )
             return
         else:
-            reply_text = ("你說了：" + user_message)
-
+            response = model.generate_content(user_message) # 傳送使用者的問題給 Gemini
+            reply_text = response.text if response else "抱歉，我無法回答這個問題。"
 
 
         line_bot_api.reply_message(
